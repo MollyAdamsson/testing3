7a6250e1c5eafca3a07183ba137827182b507d67
Molly Adamsson <adamssonmolly@gmail.com>
2023-01-23 13:17:39 +0000

    Add content

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from random import randint
 
-player_ships = [[' ']*8 for x in range(8)]
-player_guesses = [[' ']*8 for x in range(8)]
-computer_ships = [[' ']*8 for x in range(8)]
-computer_guesses = [[' ']*8 for x in range(8)]
+# Creating the player and computer's boards
+Hidden_Pattern=[[' ']*8 for x in range(8)]
+Guess_Pattern=[[' ']*8 for x in range(8)]
+Computer_Hidden_Pattern=[[' ']*8 for x in range(8)]
+Computer_Guess_Pattern=[[' ']*8 for x in range(8)]
 
 let_to_num={'A':0,'B':1, 'C':2,'D':3,'E':4,'F':5,'G':6,'H':7}
 
@@ -15,19 +16,18 @@ def print_board(board):
         print("%d|%s|" % (row_num, "|".join(row)))
         row_num +=1
 
-def get_player_guess():
+def get_ship_location():
     #Enter the row number between 1 to 8
-    row=input('Please enter a guess row 1-8 ').upper()
+    row=input('Please enter a ship row 1-8 ').upper()
     while row not in '12345678':
         print("Please enter a valid row ")
-        row=input('Please enter a guess row 1-8 ')
+        row=input('Please enter a ship row 1-8 ')
     #Enter the Ship column from A TO H
-    column=input('Please enter a guess column A-H ').upper()
+    column=input('Please enter a ship column A-H ').upper()
     while column not in 'ABCDEFGH':
         print("Please enter a valid column ")
-        column=input('Please enter a guess column A-H ')
+        column=input('Please enter a ship column A-H ')
     return int(row)-1,let_to_num[column]
-
 
 #Function that creates the ships
 def create_ships(board):
@@ -37,6 +37,9 @@ def create_ships(board):
             ship_r, ship_cl = randint(0, 7), randint(0, 7)
         board[ship_r][ship_cl] = 'X'
 
+# Creating the ships for the computer
+create_ships(Computer_Hidden_Pattern)
+
 def count_hit_ships(board):
     count=0
     for row in board:
@@ -45,45 +48,43 @@ def count_hit_ships(board):
                 count+=1
     return count
 
-create_ships(player_ships)
-create_ships(computer_ships)
+# Creating ships for the player
+create_ships(Hidden_Pattern)
+
 turns = 10
 while turns > 0:
-    print('Player Turn')
-    print_board(player_guesses)
-    player_row, player_column = get_player_guess()
-    if player_guesses[player_row][player_column] == '-':
+    print('Welcome to Battleship')
+    print_board(Guess_Pattern)
+    row,column =get_ship_location()
+    if Guess_Pattern[row][column] == '-':
         print(' You already guessed that ')
-    elif computer_ships[player_row][player_column] =='X':
-        print(' Player has hit the battleship ')
-        player_guesses[player_row][player_column] = 'X'
+    elif Computer_Hidden_Pattern[row][column] =='X':
+        print(' Congratulations you have hit the battleship ')
+        Guess_Pattern[row][column] = 'X'
+        turns -= 1
     else:
-        print('Player missed')
-        player_guesses[player_row][player_column] = '-'
-    if  count_hit_ships(player_guesses) == 5:
-        print("Player has sunk all the battleships ")
+        print('Sorry,You missed')
+        Guess_Pattern[row][column] = '-'
+        turns -= 1
+        if  count_hit_ships(Guess_Pattern) == 5:
+        print('Congratulations you have sunk all the battleships')
         break
-    print('Player has ' +str(turns) + ' turns remaining ')
-
-    # Computer's turn
-    create_ships(computer_ships)
-    create_ships(player_ships)
-    turns = 10
-    while turns > 0:
-       print('Computer Turn')
-    computer_row, computer_column = randint(0,7), randint(0,7)
-    if computer_guesses[computer_row][computer_column] == '-':
-        print('Computer already guessed that')
-    elif player_ships[computer_row][computer_column] =='X':
-        print(' Computer has hit the battleship ')
-        computer_guesses[computer_row][computer_column] = 'X'
-    else:
-        print('Computer missed')
-        computer_guesses[computer_row][computer_column] = '-'
-    if  count_hit_ships(computer_guesses) == 5:
-        print("Computer has sunk all the battleships ")
-        break
-    turns -= 1
+    print(' You have ' +str(turns) + ' turns remaining ')
     if turns == 0:
         print('Game Over ')
+        break
+
+    # Computer's turn
+    print("\nComputer's turn")
+    comp_row, comp_col = randint(0, 7), randint(0, 7)
+    if Computer_Guess_Pattern[comp_row][comp_col] == '-':
+        print("Computer already guessed that")
+    elif Hidden_Pattern[comp_row][comp_col] == 'X':
+        print("Computer hit the battleship")
+        Computer_Guess_Pattern[comp_row][comp_col] = 'X'
+    else:
+        print("Computer missed")
+        Computer_Guess_Pattern[comp_row][comp_col] = '-'
+    if count_hit_ships(Computer_Guess_Pattern) == 5:
+        print("Computer has sunk all the battleships")
         break
